cc188a87b9c579186448ac105a51a91619c15695
Alexey <oleksii.donchak@gmail.com>
2019-08-14 08:26:46 +0300

    Add more error handling

--- lib/ui/main-window.cpp
+++ lib/ui/main-window.cpp
@@ -5,11 +5,15 @@
 #include <QFile>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QMessageBox>
 
 using namespace don::notes;
 using namespace std;
 
 const QString SYMBOLS_USED_TEMPLATE("%1/%2");
+const QString INIT_ERROR("Initialization error");
+const QString ADDITION_ERROR("Addition error");
+const QString SAVE_ERROR("Initialization error");
 
 void display(
     const storage::Record& record,
@@ -38,7 +42,20 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    storage::initFrom(QStringLiteral("save.data"));
+    auto initResult = storage::initFrom(QStringLiteral("save.data"));
+    switch (initResult) {
+    case storage::InitResult::ERROR_WHILE_OPENING_FILE:
+        QMessageBox::warning(this, INIT_ERROR, QStringLiteral("Unable to open 'save.data' file. No notes loaded."));
+        break;
+    case storage::InitResult::ERROR_WHILE_READING_DATA:
+        QMessageBox::warning(this, INIT_ERROR, QStringLiteral("Unable to read data from 'save.data' file. No notes loaded."));
+        break;
+    case storage::InitResult::READ_CORRUPT_DATA:
+        QMessageBox::warning(this, INIT_ERROR, QStringLiteral("'save.data' file is corrupted. No notes loaded."));
+        break;
+    case storage::InitResult::OK:
+        break;
+    }
     ui->setupUi(this);
     connect(
         ui->noteLine, &QLineEdit::textChanged,
@@ -79,18 +96,44 @@ void MainWindow::onNoteEntered()
     auto table = ui->notesTable;
 
     const auto indexToFill = table->rowCount();
-    table->setRowCount(indexToFill + 1);
 
-    const auto result = storage::add(text);
-    if(result == storage::AdditionResult::OK){
+    const auto additionResult = storage::add(text);
+    switch (additionResult) {
+    case storage::AdditionResult::OK:{
         const auto recordVariant = storage::last();
-        const auto maybeRecord = get<storage::Record>(recordVariant);
+        const auto maybeRecord = get_if<storage::Record>(&recordVariant);
 
-        display(maybeRecord, *table, indexToFill);
+        if(maybeRecord == nullptr){
+            QMessageBox::warning(this, ADDITION_ERROR, QStringLiteral("Added note is not present in the DB."));
+            return;
+        }
+
+        table->setRowCount(indexToFill + 1);
+        display(*maybeRecord, *table, indexToFill);
+        break;
+    }
+    case storage::AdditionResult::ERROR_WHILE_ADDING_TEXT_FIELD:{
+        QMessageBox::warning(this, ADDITION_ERROR, QStringLiteral("Unable to save note text."));
+        return;
+    }
+    case storage::AdditionResult::ERROR_WHILE_ADDING_WHEN_FIELD:{
+        QMessageBox::warning(this, ADDITION_ERROR, QStringLiteral("Unable to save note date."));
+        return;
+    }
     }
 
-
-    ui->noteLine->clear();
-
-    storage::saveTo(QStringLiteral("save.data"));
+    auto saveResult = storage::saveTo(QStringLiteral("save.data"));
+    switch (saveResult) {
+    case storage::SaveResult::ERROR_WHILE_OPENING_FILE:
+        QMessageBox::warning(this, SAVE_ERROR, QStringLiteral("Unable to open 'save.data' file. No notes saved."));
+        table->setRowCount(indexToFill);
+        break;
+    case storage::SaveResult::ERROR_WHILE_WRITING_DATA:
+        QMessageBox::warning(this, SAVE_ERROR, QStringLiteral("Unable to write data to 'save.data' file. No notes saved."));
+        table->setRowCount(indexToFill);
+        break;
+    case storage::SaveResult::OK:
+        ui->noteLine->clear();
+        break;
+    }
 }
